71caee93b2323a2ea5d7300d9e700897c1e50900
mihir20 <mihir20121997@gmail.com>
2018-10-25 00:56:33 +0530

    error handled

--- app/src/main/java/in/thomso/thomsosec/MainActivity.kt
+++ app/src/main/java/in/thomso/thomsosec/MainActivity.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.media.Image
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Base64
 import android.util.Log
 import android.view.View
@@ -17,18 +16,17 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.esafirm.imagepicker.features.ImagePicker
 import com.esafirm.imagepicker.features.ReturnMode
+import com.google.zxing.integration.android.IntentIntegrator
 import id.zelory.compressor.Compressor
-import okhttp3.*
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Multipart
 import java.io.ByteArrayOutputStream
 import java.io.File
-import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -62,8 +60,9 @@ class MainActivity : AppCompatActivity() {
                 .create(Service::class.java)
 
         scanBTN.setOnClickListener {
-            val intent = Intent(this,QRScannerActivity::class.java)
-            startActivityForResult(intent,707)
+//            val intent = Intent(this,QRScannerActivity::class.java)
+//            startActivityForResult(intent,707)
+              IntentIntegrator(this).initiateScan()
         }
 
         imagePickBTN.setOnClickListener {
@@ -76,8 +75,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         sendBTN.setOnClickListener {
+            if (checkValidations()==0){
+                return@setOnClickListener
+            }
             if (image!=null){
-                val file = File(image?.path)
+                val temp = File(image?.path)
+                val file = Compressor(this).compressToFile(temp)
                 val img64 = BitmapFactory.decodeFile(file.absolutePath)
                 val outputStream = ByteArrayOutputStream()
                 img64.compress(Bitmap.CompressFormat.JPEG,20,outputStream)
@@ -86,7 +89,8 @@ class MainActivity : AppCompatActivity() {
                 //val l = outputStream.size()
                 var encode_image = ""
                // val sb = StringBuilder(encode_image)
-                encode_image += Base64.encode(b, Base64.DEFAULT)
+                encode_image += Base64.encodeToString(b, Base64.DEFAULT)
+                Log.e("IMAGE: ",encode_image)
                // val reqFile = RequestBody.create(MediaType.parse("image/*"),compressedFile)
                // val body = MultipartBody.Part.createFormData("img",file.name,reqFile)
                 val name = nameTV.text.toString()
@@ -99,18 +103,73 @@ class MainActivity : AppCompatActivity() {
 
                 val req = service.postImage(data)
 
-                req.enqueue(object :Callback<ResponseBody>{
-                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                req.enqueue(object :Callback<`in`.thomso.thomsosec.Response>{
+                    override fun onFailure(call: Call<`in`.thomso.thomsosec.Response>, t: Throwable) {
                         Log.e("ERROR",t.message)
                     }
 
-                    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                        Log.e("Success: ",response.toString())
+                    override fun onResponse(call: Call<`in`.thomso.thomsosec.Response>, response: Response<`in`.thomso.thomsosec.Response>) {
+                      //  Log.e("Success: ",response.body().toString())
+                        if (response.body()!=null&&response.body()!!.success!!){
+                            showAlert(response.body()!!.msg!!,"Success")
+                            setEveryThingEmpty()
+                        }else {
+                            showAlert(response.body()!!.msg!!,"ERROR")
+                        }
+                        //if(response.body().toString().contentEquals("success"))
                     }
 
                 })
             }
         }
+    }
+
+    private fun setEveryThingEmpty() {
+        nameTV.setText("")
+        mailTV.setText("")
+        contactTV.setText("")
+        organizationTV.setText("")
+        queryTV.setText("")
+        image = null
+        pickedImage.visibility = View.GONE
+    }
+
+    private fun checkValidations(): Int {
+        when {
+            nameTV.text.toString() == "" -> {
+                showAlert("Enter Name","ERROR")
+                return 0
+            }
+            contactTV.text.toString()=="" -> {
+                showAlert("Enter Contact Details","ERROR")
+                return 0
+            }
+            mailTV.text.toString() == "" -> {
+                showAlert("Enter Mail","ERROR")
+                return 0
+            }
+            organizationTV.text.toString() == "" -> {
+                showAlert("Enter Organization","ERROR")
+                return 0
+            }
+            queryTV.text.toString() == "" -> {
+                showAlert("Scan QR Code","ERROR")
+                return 0
+            }
+            image == null -> {
+                showAlert("Select Image","ERROR")
+                return 0
+            }
+            else -> return 1
+        }
+    }
+
+    private fun showAlert(s: String,title:String) {
+        val alert = android.support.v7.app.AlertDialog.Builder(this)
+        alert.setTitle(title)
+                .setMessage(s)
+                .setPositiveButton("ok") { dialogInterface, i -> dialogInterface.dismiss()
+                }.show()
     }
 
     private fun intiView() {
@@ -129,17 +188,25 @@ class MainActivity : AppCompatActivity() {
       //  Log.e("ACTIVE: ",data.toString())
         if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
             // or get a single image only
+//            pickedImage.visibility=View.VISIBLE
             image = ImagePicker.getFirstImageOrNull(data)
             if(image!=null){
-              //  pickedImage.visibility = View.VISIBLE
+                pickedImage.visibility = View.VISIBLE
                 Glide.with(this).load(image!!.path).into(pickedImage)
             }
+        }else{
+            val intentRes = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+            if (intentRes!=null){
+                if (intentRes.contents!=null){
+                    queryTV.setText(intentRes.contents)
+                }
+            }
         }
-        else if (data!!.hasExtra("qr")&&requestCode==707){
-            Log.e("DATA RECIEVED", data.getStringExtra("qr"))
-            queryTV.setText(data.extras.get("qr").toString())
-            queryTV.setText("text")
-        }
+//        else if (data!!.hasExtra("qr")&&requestCode==707&&resultCode==Activity.RESULT_OK){
+//            Log.e("DATA RECIEVED", data.getStringExtra("qr"))
+//            //queryTV.setText(data.extras.get("qr").toString())
+//            queryTV.setText("text")
+//        }
         super.onActivityResult(requestCode, resultCode, data)
     }
 }
---
+++ app/src/main/java/in/thomso/thomsosec/Response.java
@@ -0,0 +1,77 @@
+package in.thomso.thomsosec;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Response {
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
+    @SerializedName("body")
+    @Expose
+    private Body body;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+}
+class Body {
+
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("email")
+    @Expose
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
